0666e9acf0641b8fd9207fb4acad338a00259062
fnitu <fnitu@users.noreply.github.com>
2020-05-27 11:31:29 +0300

    Add lazy loading for application modules #8
    - remove missing scss

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class PreviewComponent implements OnInit {
