50114a2c9fa7dd8a81fae25c435d0dc40a8936bf
Nico Cauneac <cauneac.nicoleta@gmail.com>
2021-02-06 14:33:41 +0200

    User dashboad tabs style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {SharedModule} from './shared/shared.module';
-import {TranslateModule} from '@ngx-translate/core';
+import { SharedModule } from './shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
