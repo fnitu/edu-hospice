872a0cbdc9630f5738a11ffc0ae9ebd40e6d522f
Dan Nerghes <dan.nerghes@gmail.com>
2021-02-10 21:34:46 +0200

    Revert "Handle deep links from e-mails #31"
    This reverts commit b70718129d7ec405b9d3777019a977cc44ac789b.

--- src/app/shared/services/authentication/auth.service.ts
+++ src/app/shared/services/authentication/auth.service.ts
@@ -1,31 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { GLOBALS } from '../../core/globals';
 
-import {User} from '../../interfaces/user'
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
-    
-    private _userDetails: Promise<User>;
 
-    private _userDetailsResponse: Subject<User>;
-
-    constructor(private http:HttpClient){
-
-        this._userDetailsResponse = new Subject<User>();
-    }  
-
-    public get userDetails(){
-        if (!this._userDetailsResponse.observers.length){
-            this.http.get(GLOBALS.DATA_URL.USER_DETAILS).subscribe(this._userDetailsResponse);
-        }
-
-        this._userDetails = <Promise<User>> this._userDetailsResponse.toPromise();
-        return this._userDetails;
-    }
+    // FIXME
+    //  replace with server call current user
+    role: string = 'ROLE_USER';
 
     get accessToken(): string {
         return sessionStorage.getItem("token");
@@ -35,7 +17,6 @@ export class AuthService {
         sessionStorage.setItem("token", value);
     }
 
-    
     public isAuthenticated(): boolean {
         return !!this.accessToken;
     }
