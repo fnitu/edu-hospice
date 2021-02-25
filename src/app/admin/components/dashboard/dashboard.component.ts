f215c18eaf0c276bcaddad5b1013d3360a04369d
Dan Nerghes <dan.nerghes@gmail.com>
2021-02-25 23:01:38 +0200

    Integrate stats API in admin dashboard #51
    Added total number of registration that require approval, and total number of registrations awaiting payment confirmation

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ROUTES } from "../../../shared/core/routes";
-import { UserListService } from "../user-list/user-list.service";
+import { DashboardService } from 'src/app/admin/components/dashboard/dashboard.service';
+import { UserListService } from '../user-list/user-list.service';
+import { ROUTES } from '../../../shared/core/routes';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class DashboardComponent implements OnInit {
-
   public courseListRoute = `/${ROUTES.ADMIN.MAIN_ROUTE}/${ROUTES.ADMIN.COURSE_LIST}`;
 
   public usersRoute = `/${ROUTES.ADMIN.MAIN_ROUTE}/${ROUTES.ADMIN.USERS}/${this.adminService.USER_LIST_TYPES.ALL}`;
   public paymentRoute = `/${ROUTES.ADMIN.MAIN_ROUTE}/${ROUTES.ADMIN.USERS}/${this.adminService.USER_LIST_TYPES.PAYMENT}`;
   public registrationRoute = `/${ROUTES.ADMIN.MAIN_ROUTE}/${ROUTES.ADMIN.USERS}/${this.adminService.USER_LIST_TYPES.REGISTRATION}`;
 
-  constructor(private adminService: UserListService) { }
+  public registrationPending: number;
+  public paymentPending: number;
+
+  constructor(
+    private adminService: UserListService,
+    private dashboardService: DashboardService
+  ) {}
 
   ngOnInit(): void {
+    this.dashboardService.getStatusInfo().subscribe((data) => {
+      this.paymentPending = data.paymentPending;
+      this.registrationPending = data.registrationPending;
+    });
   }
-
 }
--- src/app/shared/core/globals.ts
+++ src/app/shared/core/globals.ts
@@ -1,24 +1,25 @@
-import { environment } from "../../../environments/environment";
+import { environment } from '../../../environments/environment';
 
 export const GLOBALS = {
-    DATA_URL: {
-        LOGIN: `${environment.BASE_URL}/api/auth/login`,
-        LOGOUT: `${environment.BASE_URL}/api/auth/logout`,
-        CURRENT_USER: `${environment.BASE_URL}/api/users/current-user`,
-        SEND_EMAIL_FOR_RECOVER_PASSWORD: `${environment.BASE_URL}/api/auth/send-reset-password-link`,
-        RESET_PASSWORD: `${environment.BASE_URL}/api/auth/reset-password`,
-        USER_LIST_WAITING_FOR_REGISTRATION: `${environment.BASE_URL}/api/admin/users/courses/registration-pending`,
-        USER_LIST_WAITING_FOR_PAYMENT: `${environment.BASE_URL}/api/admin/users/courses/payment-pending`,
-        USER_LIST: `${environment.BASE_URL}/api/admin/users/courses/stats`,
-        COURSES_TABS: `${environment.BASE_URL}/api/users/{userId}/courses/tabs`,
-        USER_DETAILS: `${environment.BASE_URL}/api/users/{id}/details`,
-        COURSES: `${environment.BASE_URL}/api/courses`
-    },
-    DEFAULT_COURSE_IMG: `https://www.hospice.ro/wp-content/themes/hospice/img/social-default.jpg`,
-    NOTIFICATIONS: {
-        WARNING: 'WARNING',
-        ERROR: 'ERROR',
-        INFO: 'INFO',
-        DURATION_IN_SECONDS: 10
-    }
+  DATA_URL: {
+    LOGIN: `${environment.BASE_URL}/api/auth/login`,
+    LOGOUT: `${environment.BASE_URL}/api/auth/logout`,
+    CURRENT_USER: `${environment.BASE_URL}/api/users/current-user`,
+    SEND_EMAIL_FOR_RECOVER_PASSWORD: `${environment.BASE_URL}/api/auth/send-reset-password-link`,
+    RESET_PASSWORD: `${environment.BASE_URL}/api/auth/reset-password`,
+    USER_LIST_WAITING_FOR_REGISTRATION: `${environment.BASE_URL}/api/admin/users/courses/registration-pending`,
+    USER_LIST_WAITING_FOR_PAYMENT: `${environment.BASE_URL}/api/admin/users/courses/payment-pending`,
+    USER_LIST: `${environment.BASE_URL}/api/admin/users/courses/stats`,
+    COURSES_TABS: `${environment.BASE_URL}/api/users/{userId}/courses/tabs`,
+    USER_DETAILS: `${environment.BASE_URL}/api/users/{id}/details`,
+    COURSES: `${environment.BASE_URL}/api/courses`,
+    ADMIN_DASHBOARD_STATS: `${environment.BASE_URL}/api/admin/dashboard/stats`,
+  },
+  DEFAULT_COURSE_IMG: `https://www.hospice.ro/wp-content/themes/hospice/img/social-default.jpg`,
+  NOTIFICATIONS: {
+    WARNING: 'WARNING',
+    ERROR: 'ERROR',
+    INFO: 'INFO',
+    DURATION_IN_SECONDS: 10,
+  },
 };
