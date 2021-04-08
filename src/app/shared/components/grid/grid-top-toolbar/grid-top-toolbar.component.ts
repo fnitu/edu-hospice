f000975e3e4359a07a8a33d142190cfac8ae828a
Razvan Tudorache <razvantudorache@users.noreply.github.com>
2021-04-08 19:18:43 +0300

    Add page toolbar for grid component #58
    - remove resize-observer because is not needed for the moment
    - remove code with flexMenu because is not installed in application and it's not needed

--- src/app/shared/components/grid/grid-top-toolbar/grid-top-toolbar.component.ts
+++ src/app/shared/components/grid/grid-top-toolbar/grid-top-toolbar.component.ts
@@ -1,10 +1,8 @@
 import {
   Component,
   ViewEncapsulation,
-  Input,
-  ViewChild, ElementRef, OnDestroy, AfterViewInit
+  Input, OnInit
 } from '@angular/core';
-import { ResizeObserver } from 'resize-observer';
 import {GridTopToolbarPageActionsInterface} from './grid-top-toolbar-page-actions.interface';
 
 @Component({
@@ -13,49 +11,15 @@ import {GridTopToolbarPageActionsInterface} from './grid-top-toolbar-page-action
   styleUrls: ['./grid-top-toolbar.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class GridTopToolbarComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('pageToolbarActionList') pageToolbarActionList;
+export class GridTopToolbarComponent implements OnInit {
 
   @Input() pageActions: GridTopToolbarPageActionsInterface[];
   @Input() multipleActions;
   @Input() hideToolbar: boolean = false;
 
-  private resizeObserver: ResizeObserver = null;
-
-  constructor(private host: ElementRef) {
+  constructor() {
   }
 
-  ngAfterViewInit() {
-    this.initEvents();
-  }
-
-  private initEvents() {
-    this.resizeObserver = new ResizeObserver(() => {
-
-      if (this.pageActions) {
-        this.recalculateOverflowList($(this.pageToolbarActionList.nativeElement));
-      }
-
-    });
-
-    this.resizeObserver.observe(this.host.nativeElement);
-  }
-
-  private recalculateOverflowList($list) {
-    if ($list.length) {
-      $list.flexMenu({
-        'undo': true
-      }).flexMenu({
-        'linkText': ' ',
-        'linkTextAll': ' ',
-        'linkTitle': '',
-        'linkTitleAll': '',
-        'cutoff': 0
-      });
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.resizeObserver.unobserve(this.host.nativeElement);
+  ngOnInit(): void {
   }
 }
