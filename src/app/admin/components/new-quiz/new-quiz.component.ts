2222bfc22855b0c6e40a355102330101866d0e32
Razvan Tudorache <razvantudorache@users.noreply.github.com>
2021-11-24 20:11:00 +0200

    Develop the quiz preview interface #92
    - add toolbar with buttons: quiz list, new quiz and preview quiz
    - preview quiz button is displayed only if the quiz is saved
    - add translations

--- src/app/admin/components/new-quiz/new-quiz.component.ts
+++ src/app/admin/components/new-quiz/new-quiz.component.ts
@@ -1,21 +1,69 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ROUTES } from "../../../shared/core/routes";
+import { CustomTranslateService } from "../../../shared/services/custom-translate/custom-translate.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-new-quiz',
-  templateUrl: './new-quiz.component.html',
-  styleUrls: ['./new-quiz.component.scss'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-new-quiz',
+    templateUrl: './new-quiz.component.html',
+    styleUrls: ['./new-quiz.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class NewQuizComponent implements OnInit {
-  public quizSettingSaved: boolean = false;
+    public quizSettingSaved: boolean = false;
+    public pageActions = [];
 
-  constructor() { }
+    constructor(private customTranslateService: CustomTranslateService,
+                private router: Router,
+                private route: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // if id is present on route, the quiz is saved
+        if (this.route.snapshot.params.id) {
+            this.quizSettingSaved = true;
+        }
 
-  public quizSettingsSavedEventHandler() {
-    this.quizSettingSaved = true;
-  }
+        this.pageActions = this.generatePageActions();
+    }
 
+    public quizSettingsSavedEventHandler() {
+        this.quizSettingSaved = true;
+
+        // rebuild page actions after a quiz is saved first time
+        this.pageActions = this.generatePageActions();
+    }
+
+    private generatePageActions(): {}[] {
+        let actions = [
+            {
+                label: this.customTranslateService.getTranslation('admin.quiz.showAllQuizzes'),
+                handler: () => {
+                    this.router.navigate([ROUTES.ADMIN.QUIZ.LIST], {
+                        relativeTo: this.route.parent.parent,
+                    });
+                }
+            },
+            {
+                label: this.customTranslateService.getTranslation('admin.quiz.list.addQuizButton'),
+                handler: () => {
+                    this.router.navigate([ROUTES.ADMIN.QUIZ.NEW], {
+                        relativeTo: this.route.parent,
+                    });
+                }
+            }
+        ];
+
+        // display preview button only if quiz is saved
+        if (this.quizSettingSaved) {
+            actions.push({
+                label: this.customTranslateService.getTranslation('admin.quiz.previewQuiz'),
+                handler: () => {
+                    console.log("Preview quiz");
+                }
+            });
+        }
+
+        return actions;
+    }
 }
